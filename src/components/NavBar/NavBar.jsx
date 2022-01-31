d334d7c7f8100869433441281252276fbb9f9883
Alan Tam <tam.alan7@gmail.com>
2022-01-31 12:35:46 -0800

    week 11

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,10 @@
+import {Link} from 'react-router-dom';
+
 export default function NavBar() {
-    return <nav>NavBar</nav>
+    return (
+        <nav>
+            <Link to="/orders">Order History</Link>
+            <Link to="/orders/new">New Order</Link>
+        </nav>
+    )
 }
